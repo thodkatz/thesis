ff802edd75019b364e872621462652094db23da2
Theodoros Katzalis <theodoros.katzalis@embl.de>
2024-09-10 21:54:17 +0200

    Getting started

---
+++ README.md
@@ -0,0 +1,2 @@
+# Multi-task learning in perturbational modeling
+
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+setup(
+    name="thesis",
+    version="1.0.0",
+    description="My repo for my thesis",
+    author="Theodoros Katzalis",
+    packages=find_packages(where="thesis"),
+    install_requires=[
+        "scanpy",
+        "matplotlib",
+        "seaborn",
+        "numpy",
+        "adata",
+        "pandas"
+    ],
+    extras_require={"dev": []},
+)
