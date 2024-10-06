851cb58413183cf7a9c86c64a00aacc0e9f0e3e4
Theodoros Katzalis <theodoros.katzalis@embl.de>
2024-10-06 15:10:27 +0200

    add unitednet and scbutterfly submodules

--- setup.py
+++ setup.py
@@ -13,16 +13,16 @@ setup(
         "scanpy",
         "numpy",
         "adata",
+        "pertpy",
+        "scipy<1.13", # https://discourse.pymc.io/t/importerror-cannot-import-name-gaussian-from-scipy-signal/14170, due to pertpy
+        "jax<0.4.24", # https://github.com/scverse/pertpy/issues/545, due to pertpy
     ],
     extras_require={
         "dev": [
             "matplotlib",
             "seaborn",
-            "pandas"
-        ],
-        "unifly": [
-            "jupyter",
-            "UnitedNet @ git+https://github.com/thodkatz/UnitedNet.git",
-            "scButterfly @ git+https://github.com/thodkatz/scButterfly.git"
-        ]},
+            "pandas",
+            "jupyter"
+        ]
+    },
 )
