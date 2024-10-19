497bac747ab2b2e20f84b7cfe687d1a81d748385
Theodoros Katzalis <theodoros.katzalis@embl.de>
2024-10-19 18:29:56 +0200

    Polishing
    Stop tracking runs from tensorboard

--- scripts/butterfly-sciplex-perturbation.py
+++ scripts/butterfly-sciplex-perturbation.py
@@ -1,6 +1,6 @@
 from thesis.preprocessing import preprocess_sciplex3
 from thesis import ROOT
-from thesis.scbutterfly import run_sciplex3, run_sciplex3_no_reusing
+import thesis.scbutterfly as scbutterfly
 import scanpy as sc
 
 data = ROOT / "data"
@@ -12,12 +12,12 @@ dataset2 = sciplex3[(sciplex3.obs['perturbation'] == 'Divalproex Sodium') | (sci
     
 
 def main():
-    run_sciplex3("ellagic_acid", dataset1)
-    run_sciplex3("dival_sodium", dataset2)
+    scbutterfly.run_sciplex3("ellagic_acid", dataset1)
+    scbutterfly.run_sciplex3("dival_sodium", dataset2)
     
 def main_no_reusing():
-    run_sciplex3_no_reusing("ellagic_acid_no_reusing", dataset1)
-    run_sciplex3_no_reusing("dival_sodium_no_reusing", dataset2)
+    scbutterfly.run_sciplex3_no_reusing("ellagic_acid_no_reusing", dataset1)
+    scbutterfly.run_sciplex3_no_reusing("dival_sodium_no_reusing", dataset2)
  
  
 if __name__ == "__main__":
--- thesis/scbutterfly.py
+++ thesis/scbutterfly.py
@@ -2,7 +2,7 @@ from anndata import AnnData
 from scButterfly.train_model_perturb import Model
 from scButterfly.split_datasets import unpaired_split_dataset_perturb, unpaired_split_dataset_perturb_no_reusing
 import torch.nn as nn
-from typing import List
+from typing import List, Callable
 from thesis import ROOT
 import pandas as pd
 
@@ -157,22 +157,17 @@ def run_batch(name: str, file_path: str, control: AnnData, perturb: AnnData, id_
     return pd_metrics
     
 def run_sciplex3(name: str, dataset: AnnData):
-    file_path = str(ROOT / "saved_results" / "butterfly" / "perturb" / name)
-    batch_list = list(range(0, len(dataset.obs["celltype"].cat.categories)))
-    control, perturb = prepare_data_sciplex3(dataset=dataset)
-    
-    id_list, _ = unpaired_split_dataset_perturb(control, perturb)
-    
-    pd_metrics = run_dataset(name=name, file_path=file_path, control=control, perturb=perturb, id_list=id_list, batch_list=batch_list)
-    pd_metrics.to_csv(f"{file_path}/metrics.csv")
+    _run_sciplex3(name=name, dataset=dataset, split_func=unpaired_split_dataset_perturb)
     
 def run_sciplex3_no_reusing(name: str, dataset: AnnData):
+    _run_sciplex3(name=name, dataset=dataset, split_func=unpaired_split_dataset_perturb_no_reusing)
+    
+    
+def _run_sciplex3(name: str, dataset: AnnData, split_func: Callable):
     file_path = str(ROOT / "saved_results" / "butterfly" / "perturb" / name)
     batch_list = list(range(0, len(dataset.obs["celltype"].cat.categories)))
     control, perturb = prepare_data_sciplex3(dataset=dataset)
-    
-    id_list = unpaired_split_dataset_perturb_no_reusing(control, perturb)
-    
-    pd_metrics = run_dataset(name=name, file_path=file_path, control=control, perturb=perturb, id_list=id_list, batch_list=batch_list, train=True)
+    id_list = split_func(control, perturb)
+    pd_metrics = run_dataset(name=name, file_path=file_path, control=control, perturb=perturb, id_list=id_list, batch_list=batch_list)
     pd_metrics.to_csv(f"{file_path}/metrics.csv")
  
